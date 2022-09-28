c7d8a8139c51adc1c8cdc9438c36a97cbe5ed033
Hayet Fellah <hayet.fellah145@gmail.com>
2022-09-28 01:02:11 +0100

    Populate the tasks list.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 import './style.css'
+
+const tasksList = document.querySelector('.tasks-list');
+let tasks = [
+    {
+        description : 'Buy groceries', 
+        completed : false,
+        index : 1,
+    },
+    {
+        description : 'Finish homework', 
+        completed : false,
+        index : 2,
+    },
+    {
+        description : 'Make cat food', 
+        completed : false,
+        index : 3,
+    },
+]
+
+function populateTasksSection (tasks){
+    tasks.forEach(task => {
+        const item = document.createElement('li'); 
+        item.classList.add('list-item');
+        item.innerHTML = `<button><i class="fa-regular fa-square"></i></button>
+        <span>${task.description}</span>
+        <i class="fa-solid fa-ellipsis-vertical"></i>`;
+        tasksList.append(item);
+    });
+}
+
+populateTasksSection(tasks); 
