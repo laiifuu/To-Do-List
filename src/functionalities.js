0b22bd95c25e02e06cad334b91f44e73fc5e1c2d
Hayet Fellah <hayet.fellah145@gmail.com>
2022-09-29 13:57:28 +0100

    - import updateStatus function - update the createTaskElement function

--- src/functionalities.js
+++ src/functionalities.js
@@ -1,3 +1,5 @@
+import {updateStatus} from './status.js'
+
 export default class Tasks {
   constructor() {
     this.tasksList = [];
@@ -16,53 +18,71 @@ export default class Tasks {
     const item = document.createElement('li');
     item.classList.add('list-item');
     item.setAttribute('data-index', taskObj.index);
-    item.innerHTML = `<button><i class="fa-regular fa-square"></i></button>
+    if(taskObj.completed == false){
+      item.innerHTML = `<button><i class="fa-regular fa-square checkbox"></i></button>
         <input type="text" value="${taskObj.description}" class="task-input">
         <i class="fa-solid fa-ellipsis-vertical trash-can"></i>`;
 
+        const taskInput = item.querySelector('.task-input');
+        taskInput.addEventListener('focus', () => {
+          item.style.backgroundColor = 'rgb(205, 187, 205)';
+          trashCan.classList.remove('fa-solid');
+          trashCan.classList.remove('fa-ellipsis-vertical');
+          trashCan.classList.add('fa-regular');
+          trashCan.classList.add('fa-trash-can');
+          trashCan.style.cursor = 'pointer';
+          trashCan.addEventListener('click', () => {
+            const index = parseInt(trashCan.parentElement.getAttribute('data-index'), 10);
+            this.removeTask(index);
+            trashCan.parentElement.remove();
+          });
+        });
+    
+        taskInput.addEventListener('focusout', () => {
+          item.style.backgroundColor = 'white';
+          trashCan.classList.add('fa-solid');
+          trashCan.classList.add('fa-ellipsis-vertical');
+          trashCan.classList.remove('fa-regular');
+          trashCan.classList.remove('fa-trash-can');
+          trashCan.style.cursor = 'move';
+        });
+    
+        taskInput.addEventListener('keypress', (e) => {
+          if (!e) e = window.event;
+          const keyCode = e.code || e.key;
+          if (keyCode === 'Enter') {
+            e.preventDefault();
+            const index = taskInput.parentElement.getAttribute('data-index');
+            this.tasksList[index].description = taskInput.value;
+            localStorage.setItem('tasks', JSON.stringify(this.tasksList));
+          }
+        });
+    
+        taskInput.addEventListener('keyup', (e) => {
+          if (e.keyCode === 13) {
+            e.preventDefault();
+            e.target.blur();
+          }
+        });
+
+    }
+    else{
+      item.innerHTML = `<button><i class="fa-regular fa-square-check checkbox checkbox-ticked"></i></button>
+        <span class="task-span">${taskObj.description}</span>
+        <i class="fa-solid fa-ellipsis-vertical trash-can"></i>`;
+    }
+
     const taskInput = item.querySelector('.task-input');
-    const icon = item.querySelector('.trash-can');
+    const trashCan = item.querySelector('.trash-can');
+    const checkbox = item.querySelector('.checkbox'); 
 
-    taskInput.addEventListener('focus', () => {
-      item.style.backgroundColor = 'rgb(205, 187, 205)';
-      icon.classList.remove('fa-solid');
-      icon.classList.remove('fa-ellipsis-vertical');
-      icon.classList.add('fa-regular');
-      icon.classList.add('fa-trash-can');
-      icon.style.cursor = 'pointer';
-      icon.addEventListener('click', () => {
-        const index = parseInt(icon.parentElement.getAttribute('data-index'), 10);
-        this.removeTask(index);
-        icon.parentElement.remove();
-      });
-    });
+    
 
-    taskInput.addEventListener('focusout', () => {
-      item.style.backgroundColor = 'white';
-      icon.classList.add('fa-solid');
-      icon.classList.add('fa-ellipsis-vertical');
-      icon.classList.remove('fa-regular');
-      icon.classList.remove('fa-trash-can');
-      icon.style.cursor = 'move';
-    });
-
-    taskInput.addEventListener('keypress', (e) => {
-      if (!e) e = window.event;
-      const keyCode = e.code || e.key;
-      if (keyCode === 'Enter') {
-        e.preventDefault();
-        const index = taskInput.parentElement.getAttribute('data-index');
-        this.tasksList[index].description = taskInput.value;
-        localStorage.setItem('tasks', JSON.stringify(this.tasksList));
-      }
-    });
-
-    taskInput.addEventListener('keyup', (e) => {
-      if (e.keyCode === 13) {
-        e.preventDefault();
-        e.target.blur();
-      }
-    });
+    let index = parseInt(item.getAttribute('data-index'));
+    checkbox.addEventListener('click', () => {
+      updateStatus(index, this.tasksList);
+      this.updateDisplay();
+    }); 
 
     return item;
   }
