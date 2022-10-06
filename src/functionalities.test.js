95dc007b72e0f23aee44b202926151803b2adca9
Hayet Fellah <hayet.fellah145@gmail.com>
2022-10-06 12:46:19 +0100

    Fix linter errors.

--- src/functionalities.test.js
+++ src/functionalities.test.js
@@ -1,42 +1,38 @@
-import Tasks from './functionalities';
+import Tasks from './functionalities.js';
 
-describe("Testing addTask method", () => {
-    test("The list of tasks should have 4 items", () => {
-        const tasks = new Tasks();
-        tasks.addTask("Task 0");
-        tasks.addTask("Task 1");
-        tasks.addTask("Task 2");
-        tasks.addTask("Task 3");
-        expect(tasks.tasksList.length).toBe(4);
-    });
-    test("Local storage should not be empty", () => {
-        const tasks = new Tasks();
-        tasks.addTask("Task 0");
-        tasks.addTask("Task 1");
-        tasks.addTask("Task 2");
-    expect(localStorage.getItem("tasks")).toBeTruthy();
-    });
+describe('Testing addTask method', () => {
+  test('The list of tasks should have 4 items', () => {
+    const tasks = new Tasks();
+    tasks.addTask('Task 0');
+    tasks.addTask('Task 1');
+    tasks.addTask('Task 2');
+    tasks.addTask('Task 3');
+    expect(tasks.tasksList.length).toBe(4);
+  });
+  test('Local storage should not be empty', () => {
+    const tasks = new Tasks();
+    tasks.addTask('Task 0');
+    tasks.addTask('Task 1');
+    tasks.addTask('Task 2');
+    expect(localStorage.getItem('tasks')).toBeTruthy();
+  });
 });
 
 describe('Testing removeTask method', () => {
-    test('Expect tasksList to have 1 item', () => {
-        document.body.innerHTML =
-        `<div class="tasks-section">
+  test('Expect tasksList to have 1 item', () => {
+    document.body.innerHTML = `<div class="tasks-section">
             <ul role="list" class="tasks-list">
             </ul>
         </div>
         <div class="clear-btn">
             <button disabled>Clear All Completed</button>
         </div>`;
-        const tasks = new Tasks();
-        tasks.addTask("Task 0");
-        tasks.addTask("Task 1");
-        tasks.addTask("Task 2");
-        tasks.removeTask(0);
-        tasks.removeTask(1);
-        console.log(localStorage.getItem('tasks'));
-        expect(tasks.tasksList[0].description).toBe('Task 1');
-    });
+    const tasks = new Tasks();
+    tasks.addTask('Task 0');
+    tasks.addTask('Task 1');
+    tasks.addTask('Task 2');
+    tasks.removeTask(0);
+    tasks.removeTask(1);
+    expect(tasks.tasksList[0].description).toBe('Task 1');
+  });
 });
-
-
